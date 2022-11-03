8515878c4f6faa502c340675bd53b78af617a298
Aaron Bos <aaronmbos@gmail.com>
2022-11-02 20:26:01 -0500

    Update example

--- demo/Program.cs
+++ demo/Program.cs
@@ -4,21 +4,21 @@ using System.Linq;
 
 public class Program
 {
-  public static void Main()
-  {
-    foreach (var i in Fibonacci().Take(20))
+    public static void Main()
     {
-      Console.WriteLine(i);
+        foreach (var i in Fibonacci().Take(20))
+        {
+            Console.WriteLine(i);
+        }
     }
-  }
 
-  private static IEnumerable<int> Fibonacci()
-  {
-    int current = 1, next = 1;
-    while (true) 
+    private static IEnumerable<int> Fibonacci()
     {
-      yield return current;
-      next = current + (current = next);
+        int current = 1, next = 1;
+        while (true)
+        {
+            yield return current;
+            next = current + (current = next);
+        }
     }
-  }
 }
